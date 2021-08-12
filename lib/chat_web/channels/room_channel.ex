9096df3a25962bef7b14b9a8c1112dd00e2569c1
mmsc2 <milton.scuderi@lambdaclass.com>
2021-08-12 13:20:38 -0300

    socket initiate

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -4,6 +4,7 @@ defmodule ChatWeb.RoomChannel do
   @impl true
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -21,6 +22,7 @@ defmodule ChatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert()
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
@@ -28,5 +30,14 @@ defmodule ChatWeb.RoomChannel do
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  def handle_info(:after_join, socket) do
+    Chat.Message.get_messages()
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message,
+      }) end)
+    {:noreply, socket} # :noreply
   end
 end
